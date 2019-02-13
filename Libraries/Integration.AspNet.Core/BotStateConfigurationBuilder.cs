252a3ccbecd78beef402c116d6a6b3ac75bcf40c
Drew Marsh <drmarsh@microsoft.com>
2019-02-12 21:39:23 -0800

    Initial implementation of ASP.NET Core integration
    A set of extensions that provide idiomatic configuration of various bot state features for ASP.NET Core.

---
+++ Libraries/Integration.AspNet.Core/BotStateConfigurationBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HackedBrain.BotBuilder.Integration.AspNet.Core
+{
+    public class BotStateConfigurationBuilder
+    {
+        private IServiceCollection _services;
+
+        internal BotStateConfigurationBuilder(IServiceCollection services)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+
+        public IServiceCollection Services { get => _services; }
+
+        public BotStateConfigurationBuilder UseBotState(BotState botState)
+        {
+            _services.AddSingleton<BotState>(botState);
+            _services.AddSingleton(botState.GetType(), botState);
+
+            return this;
+        }
+    }
+}
---
+++ Libraries/Integration.AspNet.Core/BotStateServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HackedBrain.BotBuilder.Integration.AspNet.Core
+{
+    public static class BotStateServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBotState(this IServiceCollection services, Action<BotStateConfigurationBuilder> config)
+        {
+            config(new BotStateConfigurationBuilder(services));
+
+            return services;
+        }
+    }
+}
